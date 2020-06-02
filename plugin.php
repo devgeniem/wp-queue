6b096a80f5f7489ed400f56834e358d280b7cd53
Ville Siltala <ville.siltala@geniem.com>
2020-06-02 09:52:23 +0300

    Add plugin.php, src directory, composer.json

---
+++ plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:       WordPress Import Controller
+ * Plugin URI:        https://github.com/devgeniem/wp-import-controller
+ * Description:       This plugin adds a modular multipurpose importer logic to WordPress.
+ * Version:           0.0.0
+ * Requires at least: 5.4
+ * Requires PHP:      7.0
+ * Author:            Geniem
+ * Author URI:        https://geniem.com/
+ * License:           GPL v3 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
