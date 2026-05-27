238efed7fd9acf19cf65bbe3bdf1d89c5366eb0a
Matias Autio <matiasautio@Matiass-MacBook-Pro.local>
2026-05-27 12:22:36 +0300

    CR changes

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
  * Plugin URI:        https://github.com/devgeniem/wp-import-controller
  * Description:       WordPress Queue is a modular library for managing queued tasks in WordPress.
  * Version:           1.0.2
- * Requires at least: 5.4
+ * Requires at least: 8.3
  * Requires PHP:      8.3
  * Author:            Geniem
  * Author URI:        https://geniem.com/
